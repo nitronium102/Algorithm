a62db098f816e1e247b59633fa7c22cefb890172
nitronium102 <nitronium@ewhain.net>
2021-07-12 00:14:57 +0900

    숫자의 합

---
+++ Strings/boj_11720.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+    char ch;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ch;
+        sum += ch - 48;
+    }
+    cout << sum << endl;
+    return 0;
+}
