23e5d2af1dcfa91ffe840cc6207a3083de098e6b
nitronium102 <nitronium@ewhain.net>
2021-07-14 22:36:24 +0900

    알파벳 찾기(find 함수)

---
+++ Strings/boj_10809.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+    for (char i = 'a'; i <= 'z'; i++)
+    {
+        cout << (int)S.find(i) << " ";
+    }
+}
