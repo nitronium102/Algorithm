acc4be4b1d467222890c71d90fdde63feca5f0aa
Minji Kang <nitronium@ewhain.net>
2021-07-25 22:41:05 +0900

    달팽이는 올라가고 싶다

---
+++ Basic_math/boj_2869.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B, V, day;
+    cin >> A >> B >> V;
+    
+    day = (V-A)/(A-B);
+    if ((V-A)%(A-B) == 0)
+        cout << day+1;
+    else
+        cout << day+2;
+}
