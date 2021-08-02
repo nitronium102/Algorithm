3503c60506c9a71c03f10b530f1c7a2be0fc461f
Minji Kang <nitronium@ewhain.net>
2021-08-03 00:00:28 +0900

    Update boj_2775.cpp

--- Basic_math/boj_2775.cpp
+++ Basic_math/boj_2775.cpp
@@ -7,20 +7,16 @@ int main(){
     for (int i=0; i<T; i++){
         cin >> K;
         cin >> N;
-        int arr[K+1][N+2] = {0,};
-        // 먼저 1층 채우기
-        for (int i=0; i<N+2; i++){
-            arr[0][i+1]=i+1;
-        }
-        int resident=0;
+        int arr[15][14]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
         // k층의 n호에 몇 명이 살고 있는가
-        for (int floor=1; floor<K+1; floor++){
-            for (int room=1; room<N+2; room++){
+        int resident;
+        for (int floor=1; floor<=K; floor++){
+            for (int room=0; room<N; room++){
                 resident += arr[floor-1][room];
                 arr[floor][room] = resident;
             }
             resident=0;
         }
-        cout << arr[K][N] << endl;
+        cout << arr[K][N-1] << endl;
     }
 }
