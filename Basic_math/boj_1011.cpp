e50cfbd7a1420bf39cbeec4b89c5947cefd859eb
Minji Kang <nitronium@ewhain.net>
2021-08-05 23:27:11 +0900

    fly me to the alpha centauri

--- Basic_math/boj_1011.cpp
+++ Basic_math/boj_1011.cpp
@@ -2,34 +2,16 @@
 using namespace std;
 
 int main(){
-    int T, x, y;
+    int T;
+    long long x, y;
     cin >> T;
     for (int i =0; i<T; i++){
         cin >> x >> y;
-        int max_distance = 1;
+        int max = 0;
         int cnt = 0;
-        int distance = y-x;
-        
-        // 1 > 2 > 1 = 1+2+1 = 4 -> 2*
-        // 1 > 2 > 3 > 2 > 1 = 1+2+3+2+1 = 9
-        while(distance>0){
-            if (distance > max_distance){
-                cnt += 1;
-                max_distance+=1;
-            }
-            else {
-                if (distance == 1){
-                    cnt += 1;
-                    max_distance = 1;
-                    break;
-                }
-                else {
-                    distance -= max_distance;
-       
-                    cnt+=1;
-                }
-            }
-        }
-        cout << cnt << endl;
-    }
+        double distance = y-x;
+        double dispow = sqrt(distance);
+        int pow = round(sqrt(distance));
+
+        if ()
 }
