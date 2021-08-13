a29dacc9f1482e08a19f34a5a9979ced089ac3c0
Minji Kang <nitronium@ewhain.net>
2021-08-13 23:31:56 +0900

    택시기하학(PI 정의)

---
+++ BaekJoon/Basic_math_2/boj_3053.cpp
@@ -0,0 +1,14 @@
+// 택시기하학
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath> // PI
+using namespace std;
+
+int main() {
+  double r;
+  cin >> r;
+  cout << fixed;
+  cout.precision(6);
+  cout << r*r*M_PI << "\n";;
+  cout << r*r*2 << "\n";
+}
