78aca34df25a53d1126ba31d57d30dcbfecc750b
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-01-23 17:13:11 +0900

    [BOJ] 수 정렬하기 / 브론즈2 / https://www.acmicpc.net/problem/2750

---
+++ BOJ/정렬/2750_수 정렬하기.cpp
@@ -0,0 +1,28 @@
+//
+// Created by KangMinji on 2023-01-12.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    vector<int> num_list;
+
+    // 입력
+    cin >> N;
+    num_list.assign(N, 0);
+    for (int i=0; i<N; i++){
+        cin >> num_list[i];
+    }
+
+    // 연산
+    sort(num_list.begin(), num_list.end());
+
+    // 출력
+    for (int i=0; i<N; i++){
+        cout << num_list[i] << "\n";
+    }
+}
