a6fc378cf85f27e56c217cb70512f6354b760e93
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-01-28 14:57:13 +0900

    [BOJ] N과M(9) / 실버2 /  https://www.acmicpc.net/problem/15663

---
+++ BOJ/백트래킹/15663_N과M(9).cpp
@@ -0,0 +1,48 @@
+// 실버2
+// Created by KangMinji on 2023-01-23.
+// https://www.acmicpc.net/problem/15663
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+vector<int> num;
+bool visited[9];
+int ans[9];
+
+void backtracking(int cnt){
+	if (cnt == M){
+		for (int i=0; i<M; i++){
+			cout << ans[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	// 이전 수열의 마지막 항과 새로운 수열의 마지막 항이 같은 경우
+	int last = -1; // 따로 만들어주지 않고 ans[cnt] 값을 쓰면 계속 덮어씌워짐
+	for (int i=0; i<N; i++){
+		if (last == num[i] || visited[i]){
+			continue;
+		}
+		ans[cnt] = last = num[i];
+		visited[i] = true;
+		backtracking(cnt+1);
+		visited[i] = false;
+	}
+}
+
+int main() {
+	// 입력
+	cin >> N >> M;
+	num.assign(N, 0);
+	for (int i=0; i<N; i++){
+		cin >> num[i];
+	}
+	sort(num.begin(), num.end());
+
+	// 연산 & 출력
+	backtracking(0);
+}
