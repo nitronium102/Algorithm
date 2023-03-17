54f3794a540eafa3528c987b3e7378b811d8d033
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-03-17 23:49:06 +0900

    [BOJ/구현] 비슷한 단어 / 실버3 / https://www.acmicpc.net/problem/2607

---
+++ BOJ/구현/2607_비슷한 단어.cpp
@@ -0,0 +1,24 @@
+// 실버3 / 구현
+// Created by KangMinji on 2023-02-10.
+// https://www.acmicpc.net/problem/2607
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+char alphabet[26];
+vector<string> word;
+
+int main() {
+	int cnt;
+
+	// 입력
+	cin >> cnt;
+	word.assign(cnt, 0);
+	for (int i=0; i<cnt; i++){
+		cin >> word[i];
+	}
+
+	// 연산
+
+}
