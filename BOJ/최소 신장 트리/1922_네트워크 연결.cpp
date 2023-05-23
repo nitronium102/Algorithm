5f8516bd4583d67103a26608da48989e52a7fcf2
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-05-23 20:35:20 +0900

    [BOJ/MST] 네트워크 연결 / 골드4 / https://www.acmicpc.net/problem/1922

---
+++ BOJ/최소 신장 트리/1922_네트워크 연결.cpp
@@ -0,0 +1,76 @@
+// 골드4
+// Created by KangMinji on 2023-05-23.
+// https://www.acmicpc.net/problem/1922
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+using namespace std;
+typedef tuple<int, int, int> tp;
+
+vector<int> parent;
+
+int findParent(int node){
+	if (parent[node] < 0){ // 값이 음수면 루트 정점
+		return node;
+	}
+	return parent[node] = findParent(parent[node]); // 그래프 압축
+}
+
+bool unionNode(int x, int y){
+	int px = findParent(x);
+	int py = findParent(y);
+
+	if (px == py){ // 같은 트리에 있다면
+		return false;
+	}
+
+	if (parent[px] < parent[py]){ // 새로운 루트 px
+		parent[px] += parent[py];
+		parent[py] = px;
+	} else { // 새로운 루트 py
+		parent[py] += parent[px];
+		parent[px] = py;
+	}
+	return true;
+}
+
+int kruskal(int n, vector<tp> &edge){
+	int sum = 0, cnt = 0;
+
+	for (auto t : edge){
+		int w, u, v;
+		tie(w, u, v) = t;
+		if (!unionNode(u, v)){ // 같은 노드에 있는 경우
+			continue;
+		}
+		sum+=w;
+		cnt++;
+
+		if (cnt == n-1){
+			return sum;
+		}
+	}
+	return 0;
+}
+
+int main() {
+	int n, m, u, v, w;
+
+	// 입력
+	cin >> n >> m;
+
+	vector<tp> edge;
+	parent.assign(n+1, -1);
+
+	while(m--){
+		cin >> u >> v >> w;
+		edge.push_back({w, u, v});
+	}
+
+	sort(edge.begin(), edge.end());
+
+	// 연산 & 출력
+	cout << kruskal(n, edge);
+}
