cf0a17c0653c001fb2e343beae223ed955b01a41
Minji Kang <68282057+nitronium102@users.noreply.github.com>
2023-07-14 23:59:54 +0900

    🐕‍🦺[BOJ/BFS] 확장게임 / 골드2 / https://www.acmicpc.net/problem/16920

--- BOJ/그래프/16920_확장 게임.cpp
+++ BOJ/그래프/16920_확장 게임.cpp
@@ -17,6 +17,7 @@ int step[10];
 int area[10];
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
+queue<tp> qlist[10];
 
 int main() {
 	// 시간
@@ -28,14 +29,14 @@ int main() {
 	for (int i = 1; i <= p; i++)
 		cin >> step[i];
 
-	vector<queue<tp>> vq(n + 1, queue<tp>());
 	for (int i = 0; i < n; i++) {
 		cin >> board[i];
 		for (int j = 0; j < m; j++) {
+			// 플레이어가 차지한 경우
 			if (board[i][j] != '.' && board[i][j] != '#') {
-				int people = board[i][j] -'0';
-				area[people]++;
-				vq[people].push({i, j, step[people]});
+				int people = board[i][j] - '0';
+				area[people] += 1;
+				qlist[people].push({i, j, step[people]});
 			}
 		}
 	}
@@ -45,25 +46,26 @@ int main() {
 		bool can_expand = false;
 
 		for (int i = 1; i <= p; i++) {
-			cout << i << "차례!\n";
 			queue<tp> nextq;
 
-			queue<tp> q = vq[i];
+			// 문제 잘못 읽음
+			// 현재 성에서 1~step[i] 거리에 있는 성들을 다 자기 영토로 만드는 것!
+			queue<tp> q = qlist[i];
 			while (!q.empty()) {
 				int x, y, cnt;
-				tie(x, y, cnt) = vq[i].front();
+				tie(x, y, cnt) = q.front();
 				q.pop();
 
 				if (cnt == 0) {
 					nextq.push({x, y, step[i]});
 					continue;
 				}
-				cout << x << ' ' << y << '\n';
-				for (int j = 0; j < 4; j++) {
-					int nx = x + dx[j] * step[i];
-					int ny = y + dy[j] * step[i];
 
-					if (nx < 0 || nx >= n || ny < 0 || ny >= 0) continue;
+				for (int j = 0; j < 4; j++) {
+					int nx = x + dx[j];
+					int ny = y + dy[j];
+
+					if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
 					if (board[nx][ny] != '.') continue;
 					board[nx][ny] = (char) (i + 48);
 					area[i]++;
@@ -71,11 +73,10 @@ int main() {
 					can_expand = true;
 				}
 			}
-			vq[i] = nextq;
+			qlist[i] = nextq;
 		}
 
 		if (!can_expand) {
-			cout << "종료합니당\n";
 			break;
 		}
 	}
